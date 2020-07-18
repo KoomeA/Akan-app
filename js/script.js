ca37ac3c7da43f777fe7c62fb9915f0aa90ae463
Edward Koome <koomeedward77@gmail.com>
2020-07-19 01:04:30 +0300

    add initial javascript

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function checkResult() {
+    let dateofBirth = document.getElementById("dateofBirth").value
+
+
+    let output = document.getElementById("result")
+    console.log("output")
+    output.innerHTML=dateofBirth;
+    
+}
