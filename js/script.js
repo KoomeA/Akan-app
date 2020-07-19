8c8da13aa0552257fb57a989dab28e6ac853fedf
Edward Koome <koomeedward77@gmail.com>
2020-07-19 21:34:59 +0300

    make changes and add initial javascript functions

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-function checkResult(){
-    let dateofBirth = document.getElementById("dateofBirth").value 
-    
-    
-}
+
+function getAkanName () {
+    let yearOfBirth = document.getElementById("year-input").value;
+    let monthOfBirth = Number(document.getElementById("month-input").value);
+    let dayOfBirth = Number(document.getElementById("day-input").value);
+    let genders = document.getElementsByName("gender");
+  
+   
