f973dbb9b5266c461052a2e503b166c4327524f7
Edward Koome <koomeedward77@gmail.com>
2020-07-19 20:03:33 +0300

    add styling and fix indentation errors

--- js/script.js
+++
@@ -1,9 +0,0 @@
-function checkResult() {
-    let dateofBirth = document.getElementById("dateofBirth").value
-
-
-    let output = document.getElementById("result")
-    console.log("output")
-    output.innerHTML=dateofBirth;
-    
-}
